eee7d4f8a13ccf16e2929e502c7915e59d1a23f1
SmashCrash69 <73446026+SmashCrash69@users.noreply.github.com>
2020-10-26 15:19:20 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,33 @@
-import discord, os, time
+import discord, time
 
-from Main import owo
+from Translator import owo
 
 client = discord.Client()
 prev_message = {}
 prefix = {}
+
+
+def prefixLog(prefix):
+    keys = []
+    with open("prefixes.txt", "a+") as file:
+        for line in file:
+            line = line.split(":")
+            keys.append(line[0])
+        for k in prefix:
+            if k in keys:
+                pass
+            else:
+                file.write(f"{k}:{prefix[k]}")
+
+
+def prefixCollect():
+    with open("prefixes.txt", "w+") as file:
+        if file.read() != "":
+            for line in file:
+                line = line.split(":")
+                prefix[line[0]] = line[1]
+        else:
+            return
 
 
 @client.event
@@ -14,60 +37,77 @@ async def on_ready():
         activity=discord.CustomActivity("OwO-ing", type="custom"),
         status=discord.Status.online,
     )
+    prefixCollect()
 
 
 @client.event
 async def on_message(message):
-    if message.guild not in list(prefix):
-        prefix[message.guild] = "!"
-    if message.author == client.user:
-        return
     with open("log.txt", "a") as log:
-        try:
-            if message.content.startswith(f"{prefix[message.guild]}owo") == False:
-                prev_message[f"{message.guild}/{message.channel}"] = message.content
-
-            if message.content.startswith(f"{prefix[message.guild]}owo"):
+        if message.guild.name != "None":
+            if message.guild.name not in list(prefix):
+                prefix[message.guild.name] = "!"
                 log.write(
-                    f"[{time.asctime(time.localtime(time.time()))}] ({message.guild}/{message.channel}/{message.author}) {message.content}\n"
+                    f"[{time.asctime(time.localtime(time.time()))}] ({message.guild.name}) server added\n"
+                )
+                prefixLog(prefix)
+        if message.author == client.user:
+            return
+        try:
+            if message.content.startswith(f"{prefix[message.guild.name]}owo") == False:
+                prev_message[f"{message.guild}/{message.channel}"] = message.content
+                return
+
+            if message.content.startswith(f"{prefix[message.guild.name]}owo"):
+                log.write(
+                    f"[{time.asctime(time.localtime(time.time()))}] ({message.guild.name}/{str(message.channel)}/{str(message.author)}) {str(message.content)}\n"
                 )
                 msg = message.content.split(" ")
                 if len(msg) > 1:
                     if msg[1].lower() == "prefix":
-                        prefix[message.guild] = msg[1].lower()
+                        print(message.guild.name)
+                        prefix[message.guild.name] = msg[2].lower()
                         await message.channel.send(
                             owo(
-                                f"The prefix has been changed to {prefix[message.guild]}"
+                                f"The prefix has been changed to {prefix[message.guild.name]}"
                             )
                         )
                         log.write(
                             f"[{time.asctime(time.localtime(time.time()))}] Complete"
                         )
+                        prefixLog(prefix)
+                        return
 
-                    if f"{message.guild}/{message.channel}" in list(prev_message):
+                    if f"{message.guild.name}/{message.channel}" in list(prev_message):
                         if msg[1].lower() == "translate":
                             await message.channel.send(
                                 owo(
-                                    prev_message[f"{message.guild}/{message.channel}"],
+                                    prev_message[
+                                        f"{message.guild.name}/{message.channel}"
+                                    ],
                                     translate=True,
                                 )
                             )
                             log.write(
                                 f"[{time.asctime(time.localtime(time.time()))}] Complete\n"
                             )
+                            return
 
                         elif msg[1].lower() == "reverse":
                             await message.channel.send(
                                 owo(
-                                    prev_message[f"{message.guild}/{message.channel}"],
+                                    prev_message[
+                                        f"{message.guild.name}/{message.channel}"
+                                    ],
                                     reverse=True,
                                 )
                             )
                             log.write(
                                 f"[{time.asctime(time.localtime(time.time()))}] Complete\n"
                             )
+                            return
                     else:
                         await message.channel.send(owo("There is nothing to owo."))
+                        return
 
                 else:
                     if f"{message.guild}/{message.channel}" in list(prev_message):
@@ -77,8 +117,10 @@ async def on_message(message):
                         log.write(
                             f"[{time.asctime(time.localtime(time.time()))}] Complete\n"
                         )
+                        return
                     else:
                         await message.channel.send(owo("There is nothing to owo."))
+                        return
         except Exception as e:
             try:
                 await message.channel.send(
@@ -87,6 +129,8 @@ async def on_message(message):
             except:
                 pass
             log.write(f"[{time.asctime(time.localtime(time.time()))}] (error) {e}\n")
+            return
 
 
-client.run("NzY5MjQxNjg4ODExNTAzNjU2.X5MKJg.c3wR82rpu3W9BiWjngCn8L40hh0")
+client.run("NzY5MjQxNjg4ODExNTAzNjU2.X5MKJg.0rqGKPrcXR6AYMvYW1dBxN84hSQ")
+
